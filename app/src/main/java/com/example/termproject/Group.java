3c09a6c654e6d453d8953272954b3d9db1a1d478
GoodestUsername <59536503+GoodestUsername@users.noreply.github.com>
2021-04-09 15:24:54 -0700

    gradle files

--- app/src/main/java/com/example/termproject/Group.java
+++ app/src/main/java/com/example/termproject/Group.java
@@ -1,3 +1,6 @@
+/*
+  Module containing group class.
+ */
 package com.example.termproject;
 
 import java.util.ArrayList;
@@ -5,6 +8,12 @@ import java.util.ArrayList;
 public class Group {
     String groupID;
     String sport;
+    String location;
+    String createdDate;
+    String lastUpdatedDate;
+    String eventDate;
+    int numberOfPeople;
+    ArrayList<String> participantIDs;
 
     public String getGroupID() {
         return groupID;
@@ -30,12 +39,28 @@ public class Group {
         this.location = location;
     }
 
-    public String getDate() {
-        return date;
+    public String getCreatedDate() {
+        return createdDate;
     }
 
-    public void setDate(String date) {
-        this.date = date;
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getLastUpdatedDate() {
+        return lastUpdatedDate;
+    }
+
+    public void setLastUpdatedDate(String lastUpdatedDate) {
+        this.lastUpdatedDate = lastUpdatedDate;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
     }
 
     public int getNumberOfPeople() {
@@ -46,39 +71,31 @@ public class Group {
         this.numberOfPeople = numberOfPeople;
     }
 
-    public ArrayList<String> getParticipantIdList() {
-        return participantIdList;
+    public ArrayList<String> getParticipantIDs() {
+        return participantIDs;
     }
 
-    public void setParticipantIdList(ArrayList<String> participantIdList) {
-        this.participantIdList = participantIdList;
+    public void setParticipantIDs(ArrayList<String> participantIDs) {
+        this.participantIDs = participantIDs;
     }
-    
+
     public boolean addToParticipantList(String idToAdd) {
-        return participantIdList.add(idToAdd);
+        return participantIDs.add(idToAdd);
     }
 
     public boolean RemoveFromParticipantList(String idToRemove) {
-        return participantIdList.add(idToRemove);
+        return participantIDs.add(idToRemove);
     }
-
-    String location;
-    String date;
-    int numberOfPeople;
-
-
-
-    ArrayList<String> participantIdList;
 
     public Group() {}
 
-    public Group(String groupID, String sport, String location, String date,
-                 int numberOfPeople, ArrayList<String> participantIdList) {
+    public Group(String groupID, String sport, String location, String createdDate,
+                 int numberOfPeople, ArrayList<String> participantIDs) {
         this.groupID = groupID;
         this.sport = sport;
         this.location = location;
-        this.date = date;
+        this.createdDate = createdDate;
         this.numberOfPeople = numberOfPeople;
-        this.participantIdList = participantIdList;
+        this.participantIDs = participantIDs;
     }
 }
